01a6900f22e8ae1748f2f92a192d7fc268d98fc4
Willian Ferreira Winck <WILLIAMFW@edu.unisinos.br>
2022-06-23 10:13:08 -0300

    Ajustando layout do teclado

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -7,34 +7,42 @@
     {{middle}}
   </div>
   <div id="bottom">
-    <button @click="mountWord(q)" class="letter">{{q}}</button>
-    <button @click="mountWord(w)" class="letter">{{w}}</button>
-    <button @click="mountWord(e)" class="letter">{{e}}</button>
-    <button @click="mountWord(r)" class="letter">{{r}}</button>
-    <button @click="mountWord(t)" class="letter">{{t}}</button>
-    <button @click="mountWord(y)" class="letter">{{y}}</button>
-    <button @click="mountWord(u)" class="letter">{{u}}</button>
-    <button @click="mountWord(i)" class="letter">{{i}}</button>
-    <button @click="mountWord(o)" class="letter">{{o}}</button>
-    <button @click="mountWord(p)" class="letter">{{p}}</button>
-    <button @click="mountWord(a)" class="letter">{{a}}</button>
-    <button @click="mountWord(s)" class="letter">{{s}}</button>
-    <button @click="mountWord(d)" class="letter">{{d}}</button>
-    <button @click="mountWord(f)" class="letter">{{f}}</button>
-    <button @click="mountWord(g)" class="letter">{{g}}</button>
-    <button @click="mountWord(h)" class="letter">{{h}}</button>
-    <button @click="mountWord(j)" class="letter">{{j}}</button>
-    <button @click="mountWord(k)" class="letter">{{k}}</button>
-    <button @click="mountWord(l)" class="letter">{{l}}</button>
-    <button @click="erraseWord()" class="letter">{{backspace}}</button>
-    <button @click="mountWord(z)" class="letter">{{z}}</button>
-    <button @click="mountWord(x)" class="letter">{{x}}</button>
-    <button @click="mountWord(c)" class="letter">{{c}}</button>
-    <button @click="mountWord(v)" class="letter">{{v}}</button>
-    <button @click="mountWord(b)" class="letter">{{b}}</button>
-    <button @click="mountWord(n)" class="letter">{{n}}</button>
-    <button @click="mountWord(m)" class="letter">{{m}}</button>
-    <button @click="submitAttemp()" class="letter">{{enter}}</button>
+    <div id="keyboard">
+      <div class="keyboard-line">
+        <button @click="mountWord(q)" class="letter btn btn-outline-primary">{{q}}</button>
+        <button @click="mountWord(w)" class="letter btn btn-outline-primary">{{w}}</button>
+        <button @click="mountWord(e)" class="letter btn btn-outline-primary">{{e}}</button>
+        <button @click="mountWord(r)" class="letter btn btn-outline-primary">{{r}}</button>
+        <button @click="mountWord(t)" class="letter btn btn-outline-primary">{{t}}</button>
+        <button @click="mountWord(y)" class="letter btn btn-outline-primary">{{y}}</button>
+        <button @click="mountWord(u)" class="letter btn btn-outline-primary">{{u}}</button>
+        <button @click="mountWord(i)" class="letter btn btn-outline-primary">{{i}}</button>
+        <button @click="mountWord(o)" class="letter btn btn-outline-primary">{{o}}</button>
+        <button @click="mountWord(p)" class="letter btn btn-outline-primary">{{p}}</button>
+      </div>
+      <div class="keyboard-line">
+        <button @click="mountWord(a)" class="letter btn btn-outline-primary">{{a}}</button>
+        <button @click="mountWord(s)" class="letter btn btn-outline-primary">{{s}}</button>
+        <button @click="mountWord(d)" class="letter btn btn-outline-primary">{{d}}</button>
+        <button @click="mountWord(f)" class="letter btn btn-outline-primary">{{f}}</button>
+        <button @click="mountWord(g)" class="letter btn btn-outline-primary">{{g}}</button>
+        <button @click="mountWord(h)" class="letter btn btn-outline-primary">{{h}}</button>
+        <button @click="mountWord(j)" class="letter btn btn-outline-primary">{{j}}</button>
+        <button @click="mountWord(k)" class="letter btn btn-outline-primary">{{k}}</button>
+        <button @click="mountWord(l)" class="letter btn btn-outline-primary">{{l}}</button>
+        <button @click="erraseWord()" id="backspace" class="btn btn-outline-primary"><i class="fa-solid fa-delete-left"></i></button>
+      </div>
+      <div class="keyboard-line">
+        <button @click="mountWord(z)" class="letter btn btn-outline-primary">{{z}}</button>
+        <button @click="mountWord(x)" class="letter btn btn-outline-primary">{{x}}</button>
+        <button @click="mountWord(c)" class="letter btn btn-outline-primary">{{c}}</button>
+        <button @click="mountWord(v)" class="letter btn btn-outline-primary">{{v}}</button>
+        <button @click="mountWord(b)" class="letter btn btn-outline-primary">{{b}}</button>
+        <button @click="mountWord(n)" class="letter btn btn-outline-primary">{{n}}</button>
+        <button @click="mountWord(m)" class="letter btn btn-outline-primary">{{m}}</button>
+        <button @click="submitAttemp()" id="enter" class="btn btn-outline-primary">{{enter}}</button>
+      </div>
+    </div>
 
   </div>
 
@@ -67,7 +75,7 @@ export default {
       j: 'J',
       k: 'K',
       l: 'L',
-      backspace: 'APAGAR',
+      backspace: '',
       z: 'Z',
       x: 'X',
       c: 'C',
@@ -117,5 +125,38 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.letter{
+  width: 3em;
+  height: 3.5em;
+  font-weight: bolder;
+  font-family: 'Font Awesome 5';
+}
+
+#enter, #backspace{
+  height: 3.5em;
+  font-weight: bolder;
+  font-family: 'Font Awesome 5';
+}
+
+#backspace{
+  width: 3em;
+}
+
+#keyboard{
+  width: 100%;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.keyboard-line{
+  padding: 0.5em;
+}
+
+.letter, #backspace, #enter{
+  margin-inline: 0.25em;
 }
 </style>
